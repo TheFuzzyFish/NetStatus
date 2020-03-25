2c36164c6dacafeb4111e008a822e3e52183d356
Zachary Kline <55845415+TheFuzzyFish@users.noreply.github.com>
2020-03-25 17:33:38 -0500

    Fixed dumb notational thing that was bugging me

--- helper-scripts/networkNotifier.py
+++ helper-scripts/networkNotifier.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python
 
 import os
+import sys
 
 # Runs NetStatus, checks to see if any services are down, and sends pushSafer.sh notification if so
 returnString = os.popen("/usr/bin/java -jar /usr/local/share/scripts/NetStatus-1.3.jar").read()
 
 # If NetStatus returns down services, send notification. Else, print "Network healthy"
 if (returnString != "Network healthy.\n"):
-	os.system("/usr/local/share/scripts/pushSafer.sh '{0}'".format(returnString))
-	print returnString
+        os.system("/usr/local/share/scripts/pushSafer.sh '{0}'".format(returnString))
+        sys.stdout.write(returnString)
 else:
-	print returnString
+        sys.stdout.write(returnString)
