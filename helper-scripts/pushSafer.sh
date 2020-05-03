36afa0e4b6163fec3a1db1a766c2ddf906c78aca
Zachary Kline <thefuzzyfish2@gmail.com>
2020-05-03 15:12:45 -0500

    Deleted helper-scripts

--- helper-scripts/pushSafer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-title="$HOSTNAME"
-body="$1"
-(/usr/bin/curl https://www.pushsafer.com/api -X POST --silent --output /dev/null -d k="FIXME" -d t="$title" -d m="$body" &)
-# FIXME: Insert your PushSafer API key into the FIXME above                            ^^^^^
